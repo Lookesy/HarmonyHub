c0cf845f2ab7a9408dd52f040c5619fd0ad13405
Lookesy <denis.titov2004@mail.ru>
2023-11-17 17:32:01 +0700

    Добавил отображение пользователя на карту

--- lib/map/domain/map_screen.dart
+++ lib/map/domain/map_screen.dart
@@ -19,61 +19,70 @@ class _MapScreenState extends State<MapScreen> with AutomaticKeepAliveClientMixi
   bool get wantKeepAlive => true;
   final mapControllerCompleter = Completer<YandexMapController>();
 
+  late final YandexMapController yandexMapController;
+
+  late PlacemarkMapObject placemarkMapObject;
+
+  CameraPosition? _userLocation;
+
+  UserLocationView? userLocationView;
+
+
+
   @override
   void initState() {
     super.initState();
     _initPermission().ignore();
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: YandexMap(
+        mapObjects: [],
         nightModeEnabled: true,
-        onMapCreated: (controller) {
+        onMapCreated: (controller) async {
+          yandexMapController = controller;
           mapControllerCompleter.complete(controller);
+          yandexMapController.toggleUserLayer(visible: true);
+          await _initPermission();
         },
-      ),
-    );
-  }
+        onUserLocationAdded: (view) async {
+          // получаем местоположение пользователя
+          _userLocation = await yandexMapController.getUserCameraPosition();
+          // если местоположение найдено, центрируем карту относительно этой точки
+          if (_userLocation != null) {
+            await yandexMapController.moveCamera(
+              CameraUpdate.newCameraPosition(
+                _userLocation!.copyWith(zoom: 15),
+              ),
+              animation: const MapAnimation(
+                type: MapAnimationType.linear,
+                duration: 0.3,
+              ),
+            );
+          }
+          // меняем внешний вид маркера - делаем его непрозрачным
+          return view.copyWith(
+            arrow: view.arrow.copyWith(isVisible: false),
+            accuracyCircle: view.accuracyCircle.copyWith(isVisible: false, strokeWidth: 0),
+            pin: view.pin.copyWith(
+              opacity: 1,
+            ),
+          );
+        },
+      ));
+    }
+
 
   /// Проверка разрешений на доступ к геопозиции пользователя
   Future<void> _initPermission() async {
     if (!await LocationService().checkPermission()) {
       await LocationService().requestPermission();
     }
-    await _fetchCurrentLocation();
   }
 
-  /// Получение текущей геопозиции пользователя
-  Future<void> _fetchCurrentLocation() async {
-    AppLatLong location;
-    const defLocation = MoscowLocation();
-    try {
-      location = await LocationService().getCurrentLocation();
-    } catch (_) {
-      location = defLocation;
-    }
-    _moveToCurrentLocation(location);
-  }
-
-  /// Метод для показа текущей позиции
-  Future<void> _moveToCurrentLocation(
-      AppLatLong appLatLong,
-      ) async {
-    (await mapControllerCompleter.future).moveCamera(
-      animation: const MapAnimation(type: MapAnimationType.linear, duration: 1),
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: Point(
-            latitude: appLatLong.lat,
-            longitude: appLatLong.long,
-          ),
-          zoom: 12,
-        ),
-      ),
-    );
-  }
 
 }
-
